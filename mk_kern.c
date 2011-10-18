2f288bc0e2b740805bcaf432c19cdfa9b2c5de5c
ec429 <ec429@cam.ac.uk>
2011-10-18 17:56:11 +0100

    Some mk_kern runs for punctuation

--- mk_kern.c
+++ mk_kern.c
@@ -1,35 +1,67 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <time.h>
+#include <ctype.h>
 
 SDL_Surface *ginit(unsigned int w, unsigned int h, unsigned char bpp);
 void pchar(SDL_Surface *scrn, unsigned int x, unsigned int y, char c);
 void pstr(SDL_Surface *scrn, unsigned int x, unsigned int y, const char *s);
 
-SDL_Surface *letters[26];
+SDL_Surface *letters[95];
 
 int main(int argc, char **argv)
 {
-	char start[2]={0,0};
+	char start[2]={1,1};
+	bool alpha=true, caps=true, punct=false, nums=false, special=false, noaa=false;
 	for(int arg=1;arg<argc;arg++)
 	{
 		if(strncmp(argv[arg], "--start=", 8)==0)
 		{
-			start[0]=argv[arg][8]-97;
-			start[1]=argv[arg][9]-97;
-			if((start[0]<0)||(start[0]>25))
+			start[0]=argv[arg][8]-32;
+			start[1]=argv[arg][9]-32;
+			if((start[0]<1)||(start[0]>94))
 			{
-				fprintf(stderr, "bad start[0] %c\n", start[0]+97);
+				fprintf(stderr, "bad start[0] %c\n", start[0]+32);
 				return(1);
 			}
-			if((start[1]<0)||(start[1]>25))
+			if((start[1]<1)||(start[1]>94))
 			{
-				fprintf(stderr, "bad start[1] %c\n", start[1]+97);
+				fprintf(stderr, "bad start[1] %c\n", start[1]+32);
 				return(1);
 			}
 		}
+		else if(strcmp(argv[arg], "-a")==0)
+			alpha=false;
+		else if(strcmp(argv[arg], "+a")==0)
+			alpha=true;
+		else if(strcmp(argv[arg], "-C")==0)
+			caps=false;
+		else if(strcmp(argv[arg], "+C")==0)
+			caps=true;
+		else if(strcmp(argv[arg], "-p")==0)
+			punct=false;
+		else if(strcmp(argv[arg], "+p")==0)
+			punct=true;
+		else if(strcmp(argv[arg], "-n")==0)
+			nums=false;
+		else if(strcmp(argv[arg], "+n")==0)
+			nums=true;
+		else if(strcmp(argv[arg], "-s")==0)
+			special=false;
+		else if(strcmp(argv[arg], "+s")==0)
+			special=true;
+		else if(strcmp(argv[arg], "-aa")==0)
+			noaa=true;
+		else if(strcmp(argv[arg], "+aa")==0)
+			noaa=false;
+	}
+	if(!(alpha||caps||punct||nums||special))
+	{
+		fprintf(stderr, "must select at least one +aCpns\n");
+		return(1);
 	}
 	srand(time(NULL));
 	SDL_Surface *screen=ginit(320, 240, 32);
@@ -38,36 +70,47 @@ int main(int argc, char **argv)
 		fprintf(stderr, "ginit: %s\n", SDL_GetError());
 		return(1);
 	}
-	for(int i=0;i<26;i++)
+	for(int i=0;i<95;i++)
 	{
-		char lfn[13];
-		sprintf(lfn, "as/as_%hhu.pbm", i+97);
+		char lfn[14];
+		sprintf(lfn, "as/as_%hhu.pbm", i+32);
 		if(!(letters[i]=IMG_Load(lfn)))
 		{
 			fprintf(stderr, "IMG_Load: %s\n", IMG_GetError());
 			return(1);
 		}
 	}
-	for(int i=start[0];i<26;i++)
+	for(int i=start[0];i<95;i++)
 	{
-		for(int j=(i==start[0]?start[1]:0);j<26;j++)
+		if(islower(i+32)) {if(!alpha) continue;}
+		else if(isupper(i+32)) {if(!caps) continue;}
+		else if(isdigit(i+32)) {if(!nums) continue;}
+		else if(ispunct(i+32)) {if(!punct) continue;}
+		else if(!special) continue;
+		for(int j=(i==start[0]?start[1]:0);j<95;j++)
 		{
+			if(islower(j+32)) {if(!alpha) continue; if(noaa&&islower(i+32)) continue;}
+			else if(isupper(j+32)) {if(!caps) continue;}
+			else if(isdigit(j+32)) {if(!nums) continue;}
+			else if(ispunct(j+32)) {if(!punct) continue;}
+			else if(!special) continue;
+			repeat:
 			SDL_FillRect(screen, &(SDL_Rect){0, 0, 320, 240}, SDL_MapRGB(screen->format, 0, 0, 0));
-			pchar(screen, 8, 8, i+97);
-			pchar(screen, 211, 8, j+97);
+			pchar(screen, 8, 8, i+32);
+			pchar(screen, 211, 8, j+32);
 			for(int y=0;y<12;y++)
 			{
 				char word[9];
 				for(int k=0;k<8;k++)
-					word[k]=97+((rand()*26.0)/RAND_MAX);
+					word[k]=32+((rand()*95.0)/RAND_MAX);
 				word[8]=0;
 				int n=1+(rand()*5.0)/RAND_MAX;
-				word[n]=i+97;
+				word[n]=i+32;
 				word[n+1]=0;
 				pstr(screen, 8, 24+(y*14), word);
 				pstr(screen, 88, 24+(y*14), word);
 				pstr(screen, 168, 24+(y*14), word);
-				word[n+1]=j+97;
+				word[n+1]=j+32;
 				pstr(screen, 13+6*n, 24+(y*14), word+n+1);
 				pstr(screen, 94+6*n, 24+(y*14), word+n+1);
 				pstr(screen, 175+6*n, 24+(y*14), word+n+1);
@@ -89,42 +132,44 @@ int main(int argc, char **argv)
 							{
 								SDL_keysym key=event.key.keysym;
 								if(key.sym==SDLK_q)
-								{
 									return(1);
-								}
+								else if(key.sym==SDLK_r)
+									goto repeat;
+								else if(key.sym==SDLK_SPACE)
+									errupt++;
 								else if(key.sym==SDLK_1)
 								{
-									printf("%c%c n\n", i+97, j+97);
+									printf("%c%c n\n", i+32, j+32);
 									errupt++;
 								}
 								else if(key.sym==SDLK_2)
 								{
-									printf("%c%c m\n", i+97, j+97);
+									printf("%c%c m\n", i+32, j+32);
 									errupt++;
 								}
 								else if(key.sym==SDLK_4)
 								{
-									printf("%c%c w\n", i+97, j+97);
+									printf("%c%c w\n", i+32, j+32);
 									errupt++;
 								}
 								else if(key.sym==SDLK_3)
 								{
-									printf("%c%c nm\n", i+97, j+97);
+									printf("%c%c nm\n", i+32, j+32);
 									errupt++;
 								}
 								else if(key.sym==SDLK_5)
 								{
-									printf("%c%c nw\n", i+97, j+97);
+									printf("%c%c nw\n", i+32, j+32);
 									errupt++;
 								}
 								else if(key.sym==SDLK_6)
 								{
-									printf("%c%c mw\n", i+97, j+97);
+									printf("%c%c mw\n", i+32, j+32);
 									errupt++;
 								}
 								else if(key.sym==SDLK_7)
 								{
-									printf("%c%c nmw\n", i+97, j+97);
+									printf("%c%c nmw\n", i+32, j+32);
 									errupt++;
 								}
 							}
@@ -167,7 +212,7 @@ SDL_Surface *ginit(unsigned int w, unsigned int h, unsigned char bpp)
 
 void pchar(SDL_Surface *scrn, unsigned int x, unsigned int y, char c)
 {
-	SDL_BlitSurface(letters[(unsigned char)c-97], NULL, scrn, &(SDL_Rect){x, y, 5, 8});
+	SDL_BlitSurface(letters[(unsigned char)c-32], NULL, scrn, &(SDL_Rect){x, y, 5, 8});
 }
 
 void pstr(SDL_Surface *scrn, unsigned int x, unsigned int y, const char *s)
